01d6c50a1257fe509684ad49f16a018052291b18
Ilya Linnik <thenixan@gmail.com>
2017-04-28 17:03:40 +0300

    Manifest downloading

--- src/main/java/ru/evotor/webbasedapplication/WebApplicationPlugin.kt
+++ src/main/java/ru/evotor/webbasedapplication/WebApplicationPlugin.kt
@@ -2,6 +2,14 @@ package ru.evotor.webbasedapplication
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.yaml.snakeyaml.Yaml
+import java.io.*
+import java.lang.IllegalArgumentException
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 /**
  * Created by nixan on 28.04.17.
@@ -9,11 +17,60 @@ import org.gradle.api.Project
 
 private const val ANDROID_PLUGIN_NAME = "com.android.application"
 
+private const val TEMPLATE_URL = "https://market.evotor.ru/static/webapptemplate/2_0_0/"
+
 class WebApplicationPlugin : Plugin<Project> {
+
+    val sourcesDirectoryPath = File("src/main/assets/")
+    val yamlFileName = "client.yaml"
 
     override fun apply(project: Project) {
         if (!project.pluginManager.hasPlugin(ANDROID_PLUGIN_NAME)) {
             throw IllegalStateException("You should also apply '$ANDROID_PLUGIN_NAME' plugin")
+        }
+
+        println("Validating template...")
+        validateTemplateLayout(sourcesDirectoryPath)
+
+    }
+
+    private fun validateTemplateLayout(path: File) {
+        if (!path.exists()) {
+            println("Path ${path.canonicalPath} doesn't exist, creating...")
+            createTemplateFolder(folderPath = path)
+        }
+
+        val manifestFile = File(path, yamlFileName)
+        if (!manifestFile.exists()) {
+            println("Downloading manifest example...")
+            createTemplateManifest(manifestPath = manifestFile)
+        }
+
+        validateTemplateManifest(manifestPath = manifestFile)
+    }
+
+    private fun createTemplateFolder(folderPath: File) {
+        folderPath.mkdirs()
+    }
+
+    private fun createTemplateManifest(manifestPath: File) {
+        downloadToFile("$TEMPLATE_URL${manifestPath.name}", manifestPath)
+    }
+
+    private fun validateTemplateManifest(manifestPath: File) {
+        val manifestYaml = Yaml().load(FileInputStream(manifestPath)).let {
+            if (it is Map<*, *>) {
+                it as Map<String, *>
+            } else {
+                throw IllegalArgumentException("Corrupt manifest file: ${manifestPath.name}")
+            }
+        }
+        manifestYaml[""]
+    }
+
+    private fun downloadToFile(url: String, file: File) {
+        URL(url).openStream().use { inputStream ->
+            Files.copy(inputStream, Paths.get(file.toURI()), StandardCopyOption.REPLACE_EXISTING)
         }
     }
 
