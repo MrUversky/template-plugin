c151f09d3d09313b7587328d4361410de05ccc34
Ilya Linnik <thenixan@gmail.com>
2017-04-28 19:04:35 +0300

    Third take on unzipping

--- src/main/java/ru/evotor/webbasedapplication/WebApplicationPlugin.kt
+++ src/main/java/ru/evotor/webbasedapplication/WebApplicationPlugin.kt
@@ -1,24 +1,14 @@
 package ru.evotor.webbasedapplication
 
-import org.apache.commons.io.IOUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.yaml.snakeyaml.Yaml
-import java.io.*
-import java.lang.IllegalArgumentException
+import org.rauschig.jarchivelib.ArchiveFormat
+import org.rauschig.jarchivelib.ArchiverFactory
 import java.net.URL
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
-import java.util.zip.ZipFile
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.util.zip.ZipEntry
-import java.util.zip.ZipOutputStream
-import java.io.IOException
 import java.io.File
-import java.util.zip.ZipInputStream
 
 
 /**
@@ -63,34 +53,12 @@ class WebApplicationPlugin : Plugin<Project> {
 
     private fun downloadTemplate(path: File) {
         val zipFile = File(path, "template.zip")
+
         downloadToFile(TEMPLATE_URL, zipFile)
-        unzip(zipFile, path)
+
+        ArchiverFactory.createArchiver(ArchiveFormat.ZIP).extract(zipFile, path)
+
         zipFile.delete()
-    }
-
-    private fun unzip(zipFile: File, outputPath: File) {
-        // buffer for read and write data to file
-        val buffer = ByteArray(1024)
-        var len: Int
-        val zis = ZipInputStream(FileInputStream(zipFile))
-        var ze: ZipEntry? = zis.nextEntry
-        while (ze != null) {
-            val fileName = ze.name
-            val newFile = File(outputPath.toString() + File.separator + fileName)
-            println("Unzipping to " + newFile.absolutePath)
-            // create directories for sub directories in zip
-            File(newFile.parent).mkdirs()
-            val fos = FileOutputStream(newFile)
-            IOUtils.copy(zis, fos)
-            fos.close()
-            zis.closeEntry()
-            ze = zis.nextEntry
-        }
-        // close last ZipEntry
-        zis.closeEntry()
-        zis.close()
-
-        println("Done!!!")
     }
 
     private fun downloadToFile(url: String, file: File) {
